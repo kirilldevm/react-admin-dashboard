78269266b3b9c9f96cd3a339c0eaa012fafeac21
kirilldev <kirill.makliuk.dev@gmail.com>
2026-01-11 23:42:58 +0200

    feat: users

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,3 +1,83 @@
+import { GridColDef } from '@mui/x-data-grid';
+import './users.scss';
+import { useEffect, useState } from 'react';
+import { userRows } from '../../data';
+import DataTable from '../../components/dataTable/data-table';
+import Add from '../../components/add/add';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'img',
+    headerName: 'Avatar',
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || '/noavatar.png'} alt='Avatar' />;
+    },
+  },
+  {
+    field: 'firstName',
+    type: 'string',
+    headerName: 'First name',
+    width: 150,
+  },
+  {
+    field: 'lastName',
+    type: 'string',
+    headerName: 'Last name',
+    width: 150,
+  },
+  {
+    field: 'email',
+    type: 'string',
+    headerName: 'Email',
+    width: 200,
+  },
+  {
+    field: 'phone',
+    type: 'string',
+    headerName: 'Phone',
+    width: 200,
+  },
+  {
+    field: 'createdAt',
+    headerName: 'Created At',
+    width: 200,
+    type: 'string',
+  },
+  {
+    field: 'verified',
+    headerName: 'Verified',
+    width: 150,
+    type: 'boolean',
+  },
+];
+
 export default function Users() {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [open]);
+  return (
+    <div className='users'>
+      <div className='info'>
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug='users' columns={columns} rows={userRows} />
+      {/* TEST THE API */}
+
+      {/* {isLoading ? (
+        "Loading..."
+      ) : (
+        <DataTable slug="users" columns={columns} rows={data} />
+      )} */}
+      {open && <Add slug='user' columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 }
